4667826d0bad2b1032a974101e6cb999a5927268
Nobsmoke123 <donaldakobundu@gmail.com>
2025-03-16 07:53:52 +0100

    feat(Pricing): integrate react-countup for animated pricing display and enhance Pricing component layout

--- src/components/Pricing/Pricing.tsx
+++ src/components/Pricing/Pricing.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Element } from "react-scroll";
 import { Plan, plans } from "./Pricing.interface";
+import CountUp from "react-countup";
+import Button from "../Button/Button";
 
 const Pricing = () => {
   const [monthly, setMonthly] = useState(false);
@@ -55,9 +57,91 @@ const Pricing = () => {
             {plans.map((plan: Plan, index: number) => (
               <div
                 key={index}
-                className="pricing-plan_first pricing-plan_last pricing-plan_odd pricing-plan_even"
+                className="pricing-plan_first pricing-plan_last pricing-plan_odd pricing-plan_even relative border-2 p-7 max-xl:min-w-80  max-lg:rounded-3xl xl:w-[calc(33.33%+2px)]"
               >
-                {plan.title}
+                {index === 1 && (
+                  <div className="g4 absolute h-330 left-0 right-0 top-0 z-1 rounded-tl-3xl rounded-tr-3xl" />
+                )}
+
+                <div
+                  className={`absolute left-0 right-0 z-2 flex items-center justify-center ${
+                    index === 1 ? "-top-6" : "-top-6 xl:-top-11"
+                  }`}
+                >
+                  <img
+                    src={plan.logo}
+                    alt={plan.title}
+                    className={`object-contain drop-shadow-2xl ${
+                      index === 1 ? "size-[120px]" : "size-[88px]"
+                    }`}
+                  />
+                </div>
+
+                <div
+                  className={`relative flex flex-col items-center ${
+                    index === 1 ? "pt-24" : "pt-12"
+                  }`}
+                >
+                  <div
+                    className={`small-2 rounded-20 relative z-2 mx-auto mb-6 border-2 px-4 py-1.5 uppercase ${
+                      index === 1 ? "border-p3 text-p3" : "border-p1 text-p1"
+                    }`}
+                  >
+                    {plan.title}
+                  </div>
+                  <div className="relative z-2 flex items-center justify-center">
+                    <div
+                      className={`h-num flex items-start ${
+                        index === 1 ? "text-p3" : "text-p4"
+                      }`}
+                    >
+                      $
+                      <CountUp
+                        start={plan.priceMonthly}
+                        end={monthly ? plan.priceMonthly : plan.priceYearly}
+                        duration={0.4}
+                        useEasing={false}
+                        preserveValue
+                      />
+                    </div>
+                    <div className="small-1 relative top-3 ml-1 uppercase">
+                      {monthly ? "/ mo" : "/ yr"}
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className={`body-1 relative z-2 mb-10 w-full border-b-s2 pb-9 text-center text-p4 ${
+                    index === 1 ? "border-b" : ""
+                  }`}
+                >
+                  {plan.caption}
+                </div>
+                <ul className={`mx-auto space-y-4 xl:px-7`}>
+                  {plan.features.map((feature, index: number) => (
+                    <li
+                      key={index}
+                      className="relative flex items-center gap-5"
+                    >
+                      <img
+                        src="/images/check.png"
+                        alt="check"
+                        className="size-10 object-contain"
+                      />
+                      <p className="flex-1"> {feature}</p>
+                    </li>
+                  ))}
+                </ul>
+                <div className="mt-10 flex w-full justify-center">
+                  <Button icon={plan.icon} onClick={() => {}}>
+                    Get Started
+                  </Button>
+                </div>
+
+                {index === 1 && (
+                  <p className="small-compact mt-9 text-center text-p3 before:mx-2.5 before:content-['-'] after:mx-2.5 after:content-['-']">
+                    Limited time offer
+                  </p>
+                )}
               </div>
             ))}
           </div>
